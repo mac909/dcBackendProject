3e13126c92421d77634615847c32aadef68fa99a
Mason McCabe <mason.mccabe2015@gmail.com>
2023-03-15 19:09:34 -0400

    Simply the original project to a regular structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const homeRoutes = require("./routes/home-routes");
+
+const app = express();
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(homeRoutes.routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
