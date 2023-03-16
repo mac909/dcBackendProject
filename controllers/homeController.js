b1d274dc9c4e298321976f9d8cae1158b5767713
Mason McCabe <mason.mccabe2015@gmail.com>
2023-03-16 16:20:38 -0400

    Create router to handle incoming api req

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,6 +2,11 @@ const indexView = (req, res, next) => {
 	res.render("home");
 };
 
+const iconView = (req, res, next) => {
+	res.render("icon");
+};
+
 module.exports = {
 	indexView,
+	iconView,
 };
