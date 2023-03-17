685f403d2d1c6bf507df9f50e97d09c6193c82cf
LucasMalone <lucasMalone@bellsouth.net>
2023-03-17 00:12:22 -0400

    adding blank pages to the links

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,11 +2,26 @@ const indexView = (req, res, next) => {
 	res.render("home");
 };
 
+const aboutView = (req, res, next) => {
+	res.render("about");
+};
+
 const iconView = (req, res, next) => {
 	res.render("icon");
+};
+
+const entreeView = (req, res, next) => {
+	res.render("entrees");
+};
+
+const ordernowView = (req, res, next) => {
+	res.render("Ordernow");
 };
 
 module.exports = {
 	indexView,
 	iconView,
+	aboutView,
+	entreeView,
+	ordernowView
 };
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { indexView, iconView } = require("../controllers/homeController");
+const { indexView, iconView, aboutView, entreeView, ordernowView } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/", indexView);
 router.get("/icon", iconView);
+router.get("/about", aboutView);
+router.get("/entrees", entreeView);
+router.get("/ordernow", ordernowView);
+
+
 
 module.exports = {
 	routes: router,
