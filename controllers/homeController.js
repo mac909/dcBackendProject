5a2c184d20bd4265daf3f4089801c6f08c3715a8
Mason McCabe <mason.mccabe2015@gmail.com>
2023-03-17 18:17:33 -0400

    Add the login and sign screen with api routing

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,7 +6,17 @@ const iconView = (req, res, next) => {
 	res.render("icon");
 };
 
+const loginView = (req, res, next) => {
+	res.render("login");
+};
+
+const signUpView = (req, res, next) => {
+	res.render("signup");
+};
+
 module.exports = {
 	indexView,
 	iconView,
+	loginView,
+	signUpView,
 };
